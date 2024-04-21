8403a072adbd1183340f6c292dd071ac58ad21ce
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-21 11:06:23 +0200

    wip 5

--- Mathlib.lean
+++ Mathlib.lean
@@ -414,6 +414,7 @@ import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Projective
 import Mathlib.Algebra.Module.Submodule.Basic
 import Mathlib.Algebra.Module.Submodule.Bilinear
+import Mathlib.Algebra.Module.Submodule.EqLocus
 import Mathlib.Algebra.Module.Submodule.Equiv
 import Mathlib.Algebra.Module.Submodule.IterateMapComap
 import Mathlib.Algebra.Module.Submodule.Ker
